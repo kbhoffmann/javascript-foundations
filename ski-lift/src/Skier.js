7bd0b5481995f498f6b4ff87d67417ba0ce2e066
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-12 15:29:58 -0600

    Add Skier class; tests pass

--- ski-lift/src/Skier.js
+++ ski-lift/src/Skier.js
@@ -1,4 +1,23 @@
 class Skier {
+  constructor(name) {
+    this.name = name
+    this.hasLiftTicket = true
+    this.skillLevel = 1
+    this.nextSlope = 'green circle'
+  }
+
+  takeLesson () {
+    ++this.skillLevel
+  }
+
+  pickSlope () {
+    if (this.skillLevel == 5) {
+      this.nextSlope = 'black diamond'
+    }
+    else if (this.skillLevel == 3) {
+      this.nextSlope = 'blue square'
+    }
+  }
 
 }
 

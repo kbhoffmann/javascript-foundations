194f84a95b7d74361794976e64d203d078e6745f
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-13 09:51:29 -0600

    Add Lift class; tests pass

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,34 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor (limit) {
+    this.inService = true
+    this.limit = limit
+    this.skiers = []
+    this.safetyBar = 'up'
+  }
+
+  admitSkier (name, ticket) {
+    if (this.skiers.length < this.limit && ticket) {
+      this.skiers.push(new Skier(name, ticket))
+    }
+
+    if (!ticket) {
+      return `Sorry, ${name}. You need a lift ticket!`
+    } else {
+      return `Sorry, ${name}. Please wait for the next lift!`
+    }
+  }
+
+  startLift () {
+    if (this.skiers.length === this.limit) {
+      this.safetyBar = 'down'
+    } else if (this.limit - this.skiers.length == 1) {
+      return `We still need 1 more skier!`
+    } else {
+      return `We still need ${this.limit - this.skiers.length} more skiers!`
+    }
+  }
 }
 
 module.exports = Lift;

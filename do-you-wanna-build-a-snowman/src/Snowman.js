43d7932820a21514095211fd318391e5fb0eb056
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-27 08:59:55 -0700

    Complete snowman class

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,17 @@
 class Snowman {
+  constructor (details) {
+    this.carrots = details.carrots
+    this.coal = details.coal
+    this.buttons = details.buttons
+    this.snowballs = details.snowballs
+    this.magicHat = false
+  }
 
+  canWearMagicHat() {
+    if(this.coal >= 2 && this.buttons >= 5 && this.carrots >= 1 && this.snowballs >= 2) {
+      this.magicHat = true
+    }
+  }
 }
 
 module.exports = Snowman;

9dd30f0e32a59e7fe37f08dd8710260dda04f585
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-28 20:33:53 -0700

    Complete Gear class

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,16 @@
 class Gear {
+  constructor (type, quantity) {
+    this.type = type
+    this.quantity = quantity
+  }
 
+  checkForValidity() {
+    if (this.type === 'mp3 player') {
+      this.type = null 
+      return 'I don\'t think a mp3 player will help us.'
+    }
+    return `Great, we\'ll need lots of ${this.type}!`
+  }
 }
 
 module.exports = Gear;

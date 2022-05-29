0f9f881b1194f69e8bf1e5c7c857e629d09e994e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-28 20:34:18 -0700

    Complete Part class

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -1,5 +1,21 @@
 class Part {
+  constructor (type) {
+    this.type = type
+    this.broken = false
+    this.breakCounter = 0
+  }
 
+  break () {
+    this.broken = true
+    this.breakCounter += 1
+  }
+
+  repair () {
+    if (this.breakCounter < 2)  {
+      this.broken = false
+    }
+    return `This ${this.type} has seen better days, we\'ll need a brand new one!`
+  }
 }
 
 module.exports = Part;

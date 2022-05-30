e2393ec618e6fad3e014ca61a4ecab35528ca922
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-29 22:04:03 -0700

    Complete Lunch box class

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
+  constructor (details) {
+    this.owner = details.owner
+    this.material = details.madeOf
+    this.shape = details.shape
+    this.color = details.color
+    this.snacks = []
+  }
 
+  acquireSnack(snack) {
+    this.snacks.push(snack)
+    snack.isInLunchBox = true
+  }
+
+  findHealthySnacks () {
+    var healthySnacks = []
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type)
+      }
+    }
+    return healthySnacks
+  }
 }
 
 module.exports = LunchBox;

987ea2e52d8a85a9cb8ce4312376beb15d48765a
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-05-29 21:22:45 -0700

    Complete Roadrace class

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,28 @@
 class Roadrace {
+  constructor (details) {
+    this.name = details.title
+    this.location = details.city
+    this.distance = 0
+    this.participants = []
+  }
 
+  setDistance (miles) {
+    this.distance += miles
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`
+  }
+
+  registerParticipants (racer) {
+    this.participants.push(racer)
+    racer.completedRaces.push(this.name)
+  }
+
+  completeRace () {
+    for (var i = 0; i < this.participants.length; i++) {
+      var racer = this.participants[i] 
+      racer.milesRun += this.distance
+      racer.fitness += this.distance
+    }
+  }
 }
 
 module.exports = Roadrace;
